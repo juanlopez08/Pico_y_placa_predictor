5f225425b5cb444bb0cd40a28c169e55b18fabf7
Juan Antonio Lopez Rodriguez <52832018+juanlopez08@users.noreply.github.com>
2020-08-22 17:57:39 -0500

    Update ControllerValidations.java

--- Pico_placa/src/controller/ControllerValidations.java
+++ Pico_placa/src/controller/ControllerValidations.java
@@ -27,17 +27,6 @@ public class ControllerValidations {
     }
 
     public int validatePicoPlaca(char lastDigit, int day) {
-        int flag = 0;
-        int lastDigitInt = Character.getNumericValue(lastDigit);
-        int pairDays[] = {0, 2, 4, 6, 8};
-        int nonPairDays[] = {1, 3, 5, 7, 9};
-
-        for (int i = 0; i < 5; i++) {
-            if ((lastDigitInt == pairDays[i] || lastDigitInt == nonPairDays[i]) && (day == i + 1)) {
-                flag = 1;
-            }
-        }
-        return flag;
-    }
+        sadsdasd
 
 }
